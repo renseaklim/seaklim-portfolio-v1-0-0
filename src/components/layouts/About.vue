484aa24b8c09a366b95adf18172da1396c53d733
renseaklim <renseaklim2002@gmail.com>
2025-07-01 14:55:23 +0700

    fix hero and about

--- src/components/layouts/About.vue
+++ src/components/layouts/About.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="md:h-[95vh] min-h-screen bg-yellow-700/20 dark:bg-slate-950/50 z-10 relative" id="about">
+    <section class="h-[60vh] md:h-[95vh] min-h-screen bg-yellow-700/20 dark:bg-slate-950/50 z-10 relative" id="about">
         <div class="container max-w-screen-xl h-full flex max-md:flex-col items-center justify-center gap-11 z-10">
             <div class="relative">
                 <div class="absolute w-full h-full peer z-10 cursor-pointer"></div>
                 <img src="@/assets/img/presentation-img2.jpg" alt="" class="max-w-[30rem] max-lg:max-w-[25rem] max-md:w-[20rem] max-sm:w-[15rem] rounded-2xl shadow-md peer-hover:rotate-12
                 transition-transform duration-500">
                 <img src="@/assets/img/presentation-img1.jpg" alt=""
-                    class="max-w-[30rem] max-lg:max-w-[25rem] max-md:w-[20rem] max-sm:w-[15rem] rounded-2xl shadow-md -z-10
+                    class="max-w-[30rem] max-lg:max-w-[25rem] max-md:w-[17rem] rounded-2xl shadow-md -z-10
                  absolute top-0 right-0 rotate-12 peer-hover:rotate-0 transition-transform duration-500 peer-hover:z-0">
             </div>
             <div class="flex flex-col gap-6 max-md:items-center justify-center">
