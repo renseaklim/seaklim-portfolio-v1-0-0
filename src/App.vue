fd3a93286677e889d799f43c69e76971d2e05d19
renseaklim <renseaklim2002@gmail.com>
2025-08-01 01:59:14 +0700

    fix aos

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,13 @@
 
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import { onMounted, nextTick, watchEffect } from "vue";
+import { onMounted, nextTick } from "vue";
 import AOS from "aos";
 import "aos/dist/aos.css";
-onMounted(async () => {
-  await nextTick()
-  AOS.init()
-})
-
-
-watchEffect(() => {
+onMounted(() => {
   nextTick(() => {
-    AOS.refresh()
-  })
-})
+    AOS.init()
+    AOS.refresh();
+  });
+});
 </script>
