ccfbd51b920b5b5d386e440fa5daac479bd60da2
renseaklim <renseaklim2002@gmail.com>
2025-08-01 01:46:26 +0700

    fix aos

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,12 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 onMounted(() => {
   nextTick(() => {
-    AOS.init({ once: true });
+    AOS.init({
+      once: true,
+      offset: 120,
+    });
     AOS.refresh();
-    window.scrollTo(0, 0); // Optional for Safari
+    window.scrollTo(0, 0); 
   });
 });
 </script>
