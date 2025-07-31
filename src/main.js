54914496dbab383ca44b4cc1f63b7f87c474e977
renseaklim <renseaklim2002@gmail.com>
2025-07-31 22:50:44 +0700

    fix aos

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import {onMounted} from 'vue'
 const app = createApp(App)
 
 app.use(createPinia())
@@ -15,6 +15,6 @@ app.use(router)
 
 app.mount('#app')
 
-AOS.init();
-
-
+onMounted(() => {
+  AOS.init();
+})
