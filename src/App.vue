e52c5a62006c3f7af97456aabfbea99d8ee73204
renseaklim <renseaklim2002@gmail.com>
2025-08-01 01:52:12 +0700

    fix aos

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,11 @@ import { RouterLink, RouterView } from "vue-router";
 import { onMounted, nextTick } from "vue";
 import AOS from "aos";
 import "aos/dist/aos.css";
-onMounted(() => {
-  nextTick(() => {
-    AOS.init({
-      once: true,
-      offset: 120,
-    });
-    AOS.refresh();
-    window.scrollTo(0, 0); 
-  });
-});
+onMounted(async () => {
+  await nextTick()
+  AOS.init({
+    once: true,
+    duration: 600,
+  })
+})
 </script>
