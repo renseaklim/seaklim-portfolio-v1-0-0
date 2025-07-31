320379d37277ed7a6d21d278b5f9eeddc59244ed
renseaklim <renseaklim2002@gmail.com>
2025-07-31 21:33:11 +0700

    fix aos

--- src/main.js
+++ src/main.js
@@ -14,16 +14,5 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
-onMounted(() => {
-  AOS.init({
-    once: true, // only animate once
-    duration: 800,
-    delay: 100,
-  });
 
-  // Re-initialize when navigating or refreshing
-  nextTick(() => {
-    AOS.refreshHard(); // force reflow
-  });
-});
 
