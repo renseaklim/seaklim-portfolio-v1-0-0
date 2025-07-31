1ccafde1ba4acd580c3c97e0921edca39975b641
renseaklim <renseaklim2002@gmail.com>
2025-07-31 22:55:46 +0700

    fix aos

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,12 @@
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { onMounted, nextTick } from 'vue'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+onMounted(() => {
+  nextTick(() => {
+    AOS.init();
+  });
+});
 </script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import './assets/style.css'
 
-import 'aos/dist/aos.css';
-import AOS from 'aos';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { nextTick, onMounted } from 'vue';
+
 const app = createApp(App)
 
 app.use(createPinia())
@@ -15,8 +13,3 @@ app.use(router)
 
 app.mount('#app')
 
-onMounted(() => {
-  nextTick(() => {
-    AOS.init();
-  });
-});
